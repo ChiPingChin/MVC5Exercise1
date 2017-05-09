1cead5940e1239d3c3ea3ab61e3bea9d56be1074
Denny Chin <denny.chin@gmail.com>
2017-05-09 21:08:55 +0800

    Add project files.

---
+++ MVC5Exercise1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5Exercise1.Startup))]
+namespace MVC5Exercise1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
