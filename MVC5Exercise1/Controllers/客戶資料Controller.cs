46ca6a0f55af422f07ec15bb4b1e55e3079e18b2
Denny Chin <denny.chin@gmail.com>
2017-05-10 21:21:36 +0800

    刪除資料功能不能真的刪除資料庫中的資料，必須修改資料庫，加上一個「是否已刪除」欄位，且列表頁不得顯示已刪除的資料。

--- MVC5Exercise1/Controllers/客戶資料Controller.cs
+++ MVC5Exercise1/Controllers/客戶資料Controller.cs
@@ -17,7 +17,13 @@ namespace MVC5Exercise1.Controllers
         // GET: 客戶資料
         public ActionResult Index()
         {
-            return View(db.客戶資料.ToList());
+            // 一次查出全部資料丟給 View 去呈現
+            //return View(db.客戶資料.ToList());
+
+            // 使用 AsQueryable()，延遲查詢(在執行 View 的 Foreach 中才會真的去資料庫查詢，效能較佳)
+            var result = db.客戶資料.Where(c => c.是否已刪除 == false).AsQueryable();
+            
+            return View(result);
         }
 
         // GET: 客戶資料/Details/5
@@ -110,7 +116,11 @@ namespace MVC5Exercise1.Controllers
         public ActionResult DeleteConfirmed(int id)
         {
             客戶資料 客戶資料 = db.客戶資料.Find(id);
-            db.客戶資料.Remove(客戶資料);
+
+            // 刪除資料功能不能真的刪除資料庫中的資料，必須修改資料庫，註記「是否已刪除」欄位為 true
+            //db.客戶資料.Remove(客戶資料);
+            客戶資料.是否已刪除 = true;
+
             db.SaveChanges();
             return RedirectToAction("Index");
         }
