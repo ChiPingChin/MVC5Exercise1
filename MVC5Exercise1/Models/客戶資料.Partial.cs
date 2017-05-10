67a2357b6c4202e11c9145db9dea6d0147a97dde
Denny Chin <denny.chin@gmail.com>
2017-05-10 22:08:27 +0800

    客戶資料欄位驗證.

--- MVC5Exercise1/Models/客戶資料.Partial.cs
+++ MVC5Exercise1/Models/客戶資料.Partial.cs
@@ -8,22 +8,30 @@ namespace MVC5Exercise1.Models
     public partial class 客戶資料
     {
     }
-    
+
+    /// <summary>
+    /// Model 欄位資料驗證
+    /// https://dotblogs.com.tw/mantou1201/2013/04/18/101814
+    /// https://dotblogs.com.tw/mantou1201/archive/2013/04/16/101657.aspx
+    /// https://dotblogs.com.tw/mantou1201/archive/2013/05/07/103108.aspx
+    /// https://dotblogs.com.tw/mantou1201/2013/09/29/122045
+    /// </summary>
     public partial class 客戶資料MetaData
     {
         [Required]
         public int Id { get; set; }
         
         [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
-        [Required]
+        [Required(ErrorMessage = "客戶名稱必填")]
         public string 客戶名稱 { get; set; }
         
+        [RegularExpression(@"^\d{8}$",ErrorMessage = "統一編號需為8位數字")]  // 統一編號需：匹配8個數字
         [StringLength(8, ErrorMessage="欄位長度不得大於 8 個字元")]
-        [Required]
+        [Required(ErrorMessage = "統一編號必填")]
         public string 統一編號 { get; set; }
         
         [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
-        [Required]
+        [Required(ErrorMessage = "電話必填")]
         public string 電話 { get; set; }
         
         [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
@@ -32,7 +40,8 @@ namespace MVC5Exercise1.Models
         [StringLength(100, ErrorMessage="欄位長度不得大於 100 個字元")]
         public string 地址 { get; set; }
         
-        [StringLength(250, ErrorMessage="欄位長度不得大於 250 個字元")]
+        [EmailAddress(ErrorMessage = "請輸入正確的電子信箱")]
+        [StringLength(250, MinimumLength = 6, ErrorMessage="欄位長度不得大於 250 個字元")]
         public string Email { get; set; }
     
         public virtual ICollection<客戶銀行資訊> 客戶銀行資訊 { get; set; }
