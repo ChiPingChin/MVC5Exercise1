cf27faf27dd78eebe8ec2154907e6729544b1bfa
Denny Chin <denny.chin@gmail.com>
2017-05-11 22:52:04 +0800

    實作一個清單頁面，顯示欄位有「客戶名稱、聯絡人數量、銀行帳戶數量」共三個欄位，此功能要在資料庫中建立一個檢視表，並且匯入 EDMX

---
+++ MVC5Exercise1/Controllers/客戶統計檢視表Controller.cs
@@ -0,0 +1,22 @@
+﻿using MVC5Exercise1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Exercise1.Controllers
+{
+    public class 客戶統計檢視表Controller : Controller
+    {
+        private CustomerEntities db = new CustomerEntities();
+
+        // GET: 客戶統計檢視表
+        public ActionResult Index()
+        {
+            var result = db.客戶統計檢視表.AsQueryable();
+
+            return View(result);
+        }
+    }
+}
